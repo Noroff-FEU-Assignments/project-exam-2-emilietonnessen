1db1b84b065bf6dea81eeaafce7fd0b43729ca1d
Emizzari <emilie@tonnessen.com>
2021-04-16 12:47:55 +0200

    chaning a classname for BEM

--- components/UI/Search/SearchBar.tsx
+++ components/UI/Search/SearchBar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 const SearchBar = () => {
     return (
-        <div className="search-bar">
+        <div className="search__bar">
             <input type="text" className="search__input" placeholder="Search.."/>
             <button className="search__button">
                 <Image src="/assets/search.svg" alt="button" width="16" height="16"  />
