d90ec0dff771d4072e5aa1dd34198e8ba11f687a
Emizzari <emilie@tonnessen.com>
2021-04-19 12:57:52 +0200

    Structuring hero

---
+++ components/Hero.tsx
@@ -0,0 +1,21 @@
+import Button from "./UI/Button";
+import Search from "./UI/Search";
+
+const Hero: React.FC = () => {
+    return (
+        <header className="hero">
+            <div className="hero__banner">
+                <div className="hero__search">
+                    <Search theme="white" />
+                </div>
+            </div>
+            <div className="hero__cta">
+                <Button link="/establishments" theme="primary" size="cta">
+                    explore
+                </Button>
+            </div>
+        </header>
+    );
+}
+
+export default Hero;
