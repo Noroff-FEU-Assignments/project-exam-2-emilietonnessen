cd1c5fad41f8bc62ad78a6f7528392049591c24f
Emizzari <emilie@tonnessen.com>
2021-04-19 16:02:45 +0200

    Update api.ts

--- constants/api.ts
+++ constants/api.ts
@@ -1,2 +1,2 @@
 export const BASE_URL: string = 'https://secure-headland-54144.herokuapp.com/';
-export const ESTABLISHMENTS_URL: string = 'https://secure-headland-54144.herokuapp.com/establishments';
+export const ESTABLISHMENTS_URL: string = 'https://secure-headland-54144.herokuapp.com/graphql';
