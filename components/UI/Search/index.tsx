2c0cb95b0452c9f6ce4a2ce7e262a23d7ecbff89
Emizzari <emilie@tonnessen.com>
2021-04-20 15:54:58 +0200

    For Connor

--- components/UI/Search/index.tsx
+++ components/UI/Search/index.tsx
@@ -1,16 +1,20 @@
+
+import React from 'react';
 import SearchBar from './SearchBar';
 import SearchResult from './SearchResult';
 import { ESTABLISHMENTS_URL } from '../../../constants/api';
 import { useState, useEffect } from 'react'
 
-
 interface SearchProps {
     theme: 'white' | 'grey';
 }
-
+ 
 const Search: React.FC<SearchProps> = ({theme}) => {
+    
+
     const [establishments, setEstablishments] = useState([]);
-    const [searchValue, setSearchValue] = useState();
+    const [searchValue, setSearchValue] = useState('');
+    const [searchIcon, setSearchIcon] = useState();
 
     useEffect(() => {
         async function fetchData() {
@@ -23,25 +27,28 @@ const Search: React.FC<SearchProps> = ({theme}) => {
             } catch (error) {
                 console.log(error)
             }
-        }; fetchData();
+        }; fetchData(); 
     }, []);
 
-    
+    const searchHandler = (event: any) => {
+        setSearchValue(event.target.value);
+    }
 
     return (
         <>
-            {console.log('Establishments', establishments);}
+            {console.log('Establishments', establishments)}
             <div className="search">
 
                 <SearchBar 
                     theme={theme}
-                    search={this.searchHandler}
+                    /* search={searchHandler}
                     value={searchValue}
                     clearSearch={this.clearSearchHandler}
-                    iconType={this.state.searchIcon}
+                    iconType={this.state.searchIcon} */
                 />
                 
                 <div className="search-results">
+                    
                     <SearchResult />
                 </div>
             </div>
