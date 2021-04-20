e908204e9de444d971636322e701961a63c6eea0
Emizzari <emilie@tonnessen.com>
2021-04-20 15:38:45 +0200

    Structuring the Establishment Card

--- components/EstablishmentCard.tsx
+++ components/EstablishmentCard.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 import Link from "next/link";
 
 interface EstablishmentCardProps {
-    link: any;
+    image: string;
     name: string;
     reviews: number;
     stars: number;
@@ -10,39 +10,37 @@ interface EstablishmentCardProps {
     slug: string;
 }
 
-const EstablishmentCard: React.FC<EstablishmentCardProps> = ({link, name, reviews, stars, price, slug }) => {
-    //console.log(link);
+const EstablishmentCard: React.FC<EstablishmentCardProps> = ({image, name, reviews, stars, price, slug }) => {
 
-    //const test = link[0];
+    // Loop through the correct amount of stars
+    const n: number = stars;
+    const calculatedStars: JSX.Element[] = [...Array(n)].map((e, i) => (
+        <Image key={i} src="/assets/icons/star-solid-grey.svg" alt={name} layout="intrinsic" width={22} height={22} />
+    ));
 
-    //console.log(test);
-    
+
     return (
-        <Link href="/">
-            <a >
+        <Link href={`/establishment/${slug}`}>
+            <a>
                 <div className="establishment-card">
                     <div className="establishment-card__img">
-                        {/* <Image src={test} alt={name} layout="responsive" width={600} height={450} /> */}
-
+                        <Image src={image} alt={name} layout="responsive" width={600} height={450} /> 
                     </div>
                     <div className="establishment-card__name">
                         {name}
                     </div>
                     <div className="establishment-card__info">
                         <div className="establishment-card__stars">
-                            {stars}
+                            {calculatedStars}
+                            <span className="establishment-card__reviews">{reviews} Reviews</span>
                         </div>
-                        <div className="establishment-card__reviews">
-                            {reviews} reviews
-                        </div>
-                        <div className="establishment-card__price">
+                        <div className="establishment-card__price-box">
                             from <span className="establishment-card__price">{price} NOK</span>
                         </div>
                     </div>
                 </div>
             </a>
         </Link>
-        
     );
 }
 
