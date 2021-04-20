85a46d1a05760f4f26b2b5d249f5b8777d88f828
Emizzari <emilie@tonnessen.com>
2021-04-20 20:56:58 +0200

    Working on establishment details page

--- pages/establishment/[slug].tsx
+++ pages/establishment/[slug].tsx
@@ -1,10 +1,61 @@
+import { GetStaticProps, GetStaticPaths } from 'next';
+import { GRAPHQL_URL, ESTABLISHMENTS_URL } from "../../constants/api";
+import Layout from '../../components/Layout';
+import axios from 'axios';
 
 const establishmentDetails = () => {
     return (
-        <div>
+        <Layout page="establishment">
             
-        </div>
-    )
+        </Layout>
+    );
 }
 
 export default establishmentDetails;
+
+
+export const getStaticPaths = async () => {
+	try {
+		const response = await axios.get(ESTABLISHMENTS_URL);
+        console.log(response);
+
+        const establishments = response.data;
+
+		const paths = establishments.map(est => ({
+			params: { 
+                id: est.slug.toString() 
+            }
+		})); 
+
+		return { 
+            paths: paths, 
+            fallback: false 
+        };
+
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+export const getStaticProps: GetStaticProps = async ({params}) => {
+    console.log(params);
+    /* const slug = params.slug;
+	const url = `${ESTABLISHMENTS_URL}/${slug}`;
+    console.log(url); */
+    //console.log(id);
+
+	/* let characters = null;
+
+	try {
+		const response = await axios.get(url);
+		characters = response.data;
+	} catch (error) {
+		console.log(error);
+	}  */
+
+	return {
+		props: { 
+            char: 'test' 
+        },
+	};
+}
