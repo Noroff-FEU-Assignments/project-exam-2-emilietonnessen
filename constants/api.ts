e529119df36b5bf56fa2f063542755120b68a27f
Emizzari <emilie@tonnessen.com>
2021-04-20 15:41:39 +0200

    Adding GraphQL URL

--- constants/api.ts
+++ constants/api.ts
@@ -1,2 +1,3 @@
 export const BASE_URL: string = 'https://secure-headland-54144.herokuapp.com/';
-export const ESTABLISHMENTS_URL: string = 'https://secure-headland-54144.herokuapp.com/graphql';
+export const GRAPHQL_URL: string = 'https://secure-headland-54144.herokuapp.com/graphql';
+export const ESTABLISHMENTS_URL: string = 'https://secure-headland-54144.herokuapp.com/establishments';
