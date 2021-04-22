53f2af7b490d83aabea543d34bc3bd552969e55e
Emizzari <emilie@tonnessen.com>
2021-04-22 16:17:40 +0200

    Structuring the details section og establishmen Details page

--- components/Details.tsx
+++ components/Details.tsx
@@ -3,27 +3,47 @@ import Image from "next/image"
 interface DetailsProps {
     description: string;
     coordinates: string;
-    amenities: any;
+    amenities: string;
 }
 
 const Details: React.FC<DetailsProps> = ({description, coordinates, amenities}) => {
     console.log(amenities);
+
+    const arrayAmenities = amenities.split('\n');
+
+    const finalAmenities: JSX.Element[] = arrayAmenities.map(am => {
+        return (
+            <li  key={am}>
+                <Image 
+                    src="/assets/icons/list-decoration.svg" 
+                    alt="List Decoration" 
+                    height={15} 
+                    width={15}  
+                />
+                {am}
+            </li>
+        );
+    });
+
     return (
         <div className="establishment-details__info details">
 
 			<p className="details__description">{description}</p>
 
-            <iframe
-                className="details__map"
-                width="100%"
-                height="400"
-                frameBorder="0"
-                scrolling="no"
-                src={`https://maps.google.com/maps?q=${coordinates}&hl=en-US&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;&output=embed`}>
-            </iframe>
+            <div className="details__map">
+                <iframe
+                    className="details__iframe"
+                    width="100%"
+                    height="max-content"
+                    frameBorder="0"
+                    scrolling="no"
+                    src={`https://maps.google.com/maps?q=${coordinates}&hl=en-US&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;&output=embed`}>
+                </iframe>
+            </div>
+            
 
 			<ul className="details__amenities">
-                {amenities}
+                {finalAmenities}
             </ul>
                 
 			<div className="details__recommendation">
@@ -31,10 +51,10 @@ const Details: React.FC<DetailsProps> = ({description, coordinates, amenities}) 
                     Lucy and other 3 friends recommend this hotel
                 </p>
                 <div className="details__recommendation-images">
-                    <Image src="/assets/users/female-1.jpg" alt="User 1" width={40} height={40} />
-                    <Image src="/assets/users/female-2.jpg" alt="User 2" width={40} height={40} />
-                    <Image src="/assets/users/male-1.jpg" alt="User 3" width={40} height={40} />
-                    <Image src="/assets/users/male-2.jpg" alt="User 4" width={40} height={40} />
+                    <Image src="/assets/users/female-1.jpg" alt="User 1" width={45} height={45} />
+                    <Image src="/assets/users/female-2.jpg" alt="User 2" width={45} height={45} />
+                    <Image src="/assets/users/male-1.jpg" alt="User 3" width={45} height={45} />
+                    <Image src="/assets/users/male-2.jpg" alt="User 4" width={45} height={45} />
                 </div>
             </div>
 		</div>
