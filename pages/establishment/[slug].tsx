a08bb3c972b82ae99b2db8647e8de5b411f5daf3
Emizzari <emilie@tonnessen.com>
2021-04-23 12:16:11 +0200

    Cleaning up the code and adding a Modal

--- pages/establishment/[slug].tsx
+++ pages/establishment/[slug].tsx
@@ -1,17 +1,21 @@
-import { GetStaticProps, GetStaticPaths } from 'next';
-import { ESTABLISHMENTS_URL } from "../../constants/api";
-import Layout from '../../components/Layout';
-import axios, { AxiosResponse } from 'axios';
 import Image from 'next/image';
+import axios, { AxiosResponse } from 'axios';
+import { Params } from 'next/dist/next-server/server/router';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import { GetStaticProps } from 'next';
+import { ESTABLISHMENTS_URL, GRAPHQL_URL } from "../../constants/api";
+import { ModalButton } from '../../components/UI/Button';
+import Layout from '../../components/Layout';
 import Details from '../../components/Details';
 import Rooms from '../../components/Rooms';
 import EstablishmentReviews from '../../components/EstablishmentReviews';
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
-import { GRAPHQL_URL } from "../../constants/api";
+import BookingEnquiry from '../../components/BookingEnquiry';
 
 interface Address {
 	coordinates: string;
 	street: string;
+	city: string;
+	zipcode: number;
 }
 
 interface Gallery {
@@ -24,17 +28,20 @@ interface Gallery {
 
 interface Establishment {
 	id: number;
-        slug: string;
-        name: string;
-        stars: number;
-		rating: number;
-        featured: boolean;
-        reviews: string;
-        lowestPrice: string;
-		description: string;
-		amenities: string;
-		address: Address;
-		gallery: Gallery[];
+    slug: string;
+    name: string;
+	email: string;
+	phone: number;
+
+    stars: number;
+	rating: number;
+    featured: boolean;
+    reviews: string;
+    lowestPrice: string;
+	description: string;
+	amenities: string;
+	address: Address;
+	gallery: Gallery[];
 }
 
 interface EstablishmentDetailsProps {
@@ -42,6 +49,8 @@ interface EstablishmentDetailsProps {
 		id: number;
         slug: string;
         name: string;
+		email: string;
+		phone: number;
         stars: number;
 		rating: number;
         featured: boolean;
@@ -56,91 +65,98 @@ interface EstablishmentDetailsProps {
 
 const establishmentDetails: React.FC<EstablishmentDetailsProps> = ({est}) => {
 	
-	//console.log(typeof(est));
+	//console log the establishment endpoints
 	console.log('[EST]', est);
 
+	// Format the gallery into a JSX element
 	const gallery = est.gallery;
+	const formattedGallery: JSX.Element[] = gallery.map(img => (
+		<div className="establishment-gallery__box" key={img.id}>
+			<Image 
+				className="establishment-gallery__img"
+				src={img.url} 
+				alt={img.alternativeText} 
+				layout="responsive" 
+				width={img.width} 
+				height={img.height}  />
+		</div>
+	)); 
 
-	const formattedGallery: JSX.Element[] = gallery.map(img => {
-		return (
-			<div className="establishment-gallery__box" key={img.id}>
-				<Image 
-					className="establishment-gallery__img"
-					src={img.url} 
-					alt={img.alternativeText} 
-					layout="responsive" 
-					width={img.width} 
-					height={img.height}
-				/>
-			</div>
-		)
-	}); 
-
+	// Calculate the amount of stars that shall be displayed
 	const n: number = est.stars;
     const calculatedStars: JSX.Element[] = [...Array(n)].map((e, i) => (
-        <Image key={i} src="/assets/icons/star-solid-primary.svg" alt="star" layout="intrinsic" width={22} height={22} />
+        <Image 
+			key={i} 
+			src="/assets/icons/star-solid-primary.svg" 
+			alt="star" 
+			layout="intrinsic" 
+			width={22} 
+			height={22} />
     ));
 
-	// Coordinates
+	// Removing any spaces in the coordinates
 	const coordinates: string = est.address.coordinates;
 	const filteredCoordinates: string = coordinates.replace(/ /g,'');
 
+	// Calculate a random room number left between 1 and 10
+	const roomsLeft: number = Math.floor((Math.random() * 10) + 1);
+
     return (
-        <Layout page="establishment">
-			{/* Gallery: */}
-            <section className="establishment-gallery">
-				{formattedGallery} 
-			</section>
+		<>
+			<Layout page="establishment">
+				{/* Gallery: */}
+				<section className="establishment-gallery">
+					{formattedGallery} 
+				</section>
 
-			{/* Header: */}
-			<header className="establishment-header">
-
-				<div className="establishment-header__box">
-					<h2 className="establishment-header__title">{est.name}</h2>
-
-					<div className="establishment-header__stars">{calculatedStars}</div>
-				</div>
-
-				<div className="establishment-header__box">
-					<div className="establishment-header__address">
-						<Image src="/assets/icons/address-primary.svg" alt="star" layout="intrinsic" width={15} height={15} />
-						{est.address.street}
+				{/* Header: */}
+				<header className="establishment-header">
+					<div className="establishment-header__box">
+						<h2 className="establishment-header__title">{est.name}</h2>
+						<div className="establishment-header__stars">{calculatedStars}</div>
 					</div>
-
-					<div className="establishment-header__rating-box">
-						<div>
-							<div className="establishment-header__rating">{est.rating}</div>
-							<div className="establishment-header__reviews">{est.reviews} reviews</div>
+					<div className="establishment-header__box">
+						<div className="establishment-header__address">
+							<Image src="/assets/icons/address-primary.svg" alt="star" layout="intrinsic" width={15} height={15} />
+							{est.address.street}
+						</div>
+						<div className="establishment-header__rating-box">
+							<div>
+								<div className="establishment-header__rating">{est.rating}</div>
+								<div className="establishment-header__reviews">{est.reviews} reviews</div>
+							</div>
 						</div>
 					</div>
-				</div>
+				</header>
+
+				{/* Details: */}
+				<section className="establishment-details">
+					<Details 
+						coordinates={filteredCoordinates}
+						description={est.description}
+						amenities={est.amenities} />
+					<Rooms />
+					<EstablishmentReviews />
+				</section>
+
+				{/* Booking */}
+				<section className="establishment-booking booking" id="booking">
+					<h3 className="booking__title">Good news we have {roomsLeft} rooms left!</h3>
+					<ModalButton theme="primary" size="cta">
+						book now
+					</ModalButton>
+				</section>
 				
+			</Layout>
 
-				
-			</header>
-
-			{/* Details: */}
-			<section className="establishment-details">
-				<Details 
-					coordinates={filteredCoordinates}
-					description={est.description}
-					amenities={est.amenities} />
-
-				<Rooms />
-
-				<EstablishmentReviews />
-			</section>
-
-			{/* Booking */}
-			<section className="establishment-booking">
-				Booking
-			</section>
-
-			{/* Booking Modal: */}
-			<div className="modal">
-				Modal
-			</div>
-        </Layout>
+			{/* Booking Enquiry Modal: */}
+			<BookingEnquiry
+				email={est.email}
+				phone={est.phone}
+				street={est.address.street}
+				city={est.address.city}
+				zipcode={est.address.zipcode} />
+		</>
     );
 }
 
@@ -148,16 +164,15 @@ export default establishmentDetails;
 
 
 
-
-
-export const getStaticPaths = async () => {
+// Get the slug from the API
+export const getStaticPaths: () => Promise<{ paths: { params: { slug: string; }; }[]; fallback: boolean; } | undefined> = async () => {
 	try {
 		const response: AxiosResponse<any> = await axios.get(ESTABLISHMENTS_URL);
         const establishments: any[] = response.data;
 
 		const paths: {params: {slug: string}}[]  = establishments.map((est) => ({
 			params:  { 
-				slug: est.slug,
+				slug: est.slug.toString(),
 			},
 		}));
 
@@ -173,66 +188,63 @@ export const getStaticPaths = async () => {
 
 
 
-
-export const getStaticProps: GetStaticProps = async ({params}) => {
+// Based on the acquired slug - the graphQL will pick up the necessary endpoints
+export const getStaticProps: GetStaticProps = async ({ params }: Params) => {
 	let slug: string | string[] | undefined;
     
+	// Going around a TS error that didn't liked that the value could be "undefined"
 	if (params) {
 		slug = params.slug;
 	}
 
-	let estGraphQL: Establishment[] = [];
+	let establishment: Establishment[] = [];
 
 	const client = new ApolloClient({
         uri: GRAPHQL_URL,
         cache: new InMemoryCache
     });
 
+	// Fetching the needed endpoints based on the slug
 	const { data } = await client.query({
-        query: gql`
-            query  {
-                establishments (where: {slug: "${slug}"}) {
-                    id
-                    slug
-                    name
-                    stars
-					rating
-                    featured
-                    reviews
-                    lowestPrice
-					description
-					email
-					phone
-					category
-					amenities
-					address {
-						coordinates
-						street
-						zipcode
-						city
-					}
-					gallery {
-						id
-						alternativeText
-						url
-						width
-						height
-					}
+        query: gql` query  {
+            establishments (where: {slug: "${slug}"}) {
+                id
+                slug
+                name
+                stars
+				rating
+                featured
+                reviews
+                lowestPrice
+				description
+				email
+				phone
+				category
+				amenities
+				address {
+					coordinates
+					street
+					zipcode
+					city
+				}
+				gallery {
+					id
+					alternativeText
+					url
+					width
+					height
+				}
                     
-                }                   
-            }
-        `
-    });
+            }                   
+    	}`
+	});
 
-	estGraphQL = data.establishments;
+	establishment = data.establishments;
 
-	
-
-	// GraphLQ Testing
 
 	return {
 		props: { 
-            est: estGraphQL[0]
+            est: establishment[0]
         },
 	};
 } 
