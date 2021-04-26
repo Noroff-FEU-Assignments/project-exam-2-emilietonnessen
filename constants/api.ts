e9db9d638a02cc33d86cdceab62a3b8b676e8470
Emizzari <emilie@tonnessen.com>
2021-04-26 15:43:00 +0200

    Adding Enquiries api url

--- constants/api.ts
+++ constants/api.ts
@@ -1,3 +1,4 @@
 export const BASE_URL: string = 'https://secure-headland-54144.herokuapp.com/';
 export const GRAPHQL_URL: string = 'https://secure-headland-54144.herokuapp.com/graphql';
 export const ESTABLISHMENTS_URL: string = 'https://secure-headland-54144.herokuapp.com/establishments';
+export const ENQUIRY_URL: string = 'https://secure-headland-54144.herokuapp.com/enquiries';
