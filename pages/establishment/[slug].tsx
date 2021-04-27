7cd30401aefd14a8877d48ece3e4912885414cd1
Emizzari <emilie@tonnessen.com>
2021-04-27 14:01:48 +0200

    Cleaning up the code

--- pages/establishment/[slug].tsx
+++ pages/establishment/[slug].tsx
@@ -1,17 +1,18 @@
-import Image from 'next/image';
 import axios, { AxiosResponse } from 'axios';
 import { Params } from 'next/dist/next-server/server/router';
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 import { GetStaticProps } from 'next';
-import { ESTABLISHMENTS_URL, GRAPHQL_URL } from "../../constants/api";
-import { ModalButton } from '../../components/UI/Button';
-import Layout from '../../components/Layout';
-import Details from '../../components/Details';
-import Rooms from '../../components/Rooms';
-import EstablishmentReviews from '../../components/EstablishmentReviews';
-import BookingEnquiry from '../../components/BookingEnquiry';
-import Head from 'next/head';
 
+import { ESTABLISHMENTS_URL, GRAPHQL_URL } from "../../constants/api";
+import Layout from '../../components/Layout';
+import Booking from '../../components/Booking';
+import { BookingFeedbackSuccess, BookingFeedbackError } from '../../components/Booking/BookingFeedback';
+import EstablishmentGallery from '../../components/Establihsment/EstablishmentGallery';
+import EstablishmentHeader from '../../components/Establihsment/EstablishmentHeader';
+import EstablishmentInfo from '../../components/Establihsment/EstablishmentInfo';
+import EstablishmentCTA from '../../components/Establihsment/EstablishmentCTA';
+
+// Interfaces
 interface Address {
 	coordinates: string;
 	street: string;
@@ -20,11 +21,11 @@ interface Address {
 }
 
 interface Gallery {
-	id: number;
 	alternativeText: string;
-	url: string;
-	width: string;
-	height: string;
+    height: number;
+    width: number;
+    url: string;
+    id: string;
 }
 
 interface Establishment {
@@ -33,11 +34,10 @@ interface Establishment {
     name: string;
 	email: string;
 	phone: number;
-
     stars: number;
 	rating: number;
     featured: boolean;
-    reviews: string;
+    reviews: number;
     lowestPrice: string;
 	description: string;
 	amenities: string;
@@ -46,112 +46,53 @@ interface Establishment {
 }
 
 interface EstablishmentDetailsProps {
-	est: {
-		id: number;
-        slug: string;
-        name: string;
-		email: string;
-		phone: number;
-        stars: number;
-		rating: number;
-        featured: boolean;
-        reviews: string;
-        lowestPrice: string;
-		description: string;
-		amenities: string;
-		address: Address;
-		gallery: Gallery[];
-	};
+	est: Establishment;
 }
 
+
+
+
 const establishmentDetails: React.FC<EstablishmentDetailsProps> = ({est}) => {
-	
-	//console log the establishment endpoints
-	//console.log('[EST]', est);
-	console.log('[Establishment Name]', est.name);
-
-	// Format the gallery into a JSX element
-	const gallery = est.gallery;
-	const formattedGallery: JSX.Element[] = gallery.map(img => (
-		<div className="establishment-gallery__box" key={img.id}>
-			<Image 
-				className="establishment-gallery__img"
-				src={img.url} 
-				alt={img.alternativeText} 
-				layout="responsive" 
-				width={img.width} 
-				height={img.height}  />
-		</div>
-	)); 
-
-	// Calculate the amount of stars that shall be displayed
-	const n: number = est.stars;
-    const calculatedStars: JSX.Element[] = [...Array(n)].map((e, i) => (
-        <Image 
-			key={i} 
-			src="/assets/icons/star-solid-primary.svg" 
-			alt="star" 
-			layout="intrinsic" 
-			width={22} 
-			height={22} />
-    ));
-
-	// Removing any spaces in the coordinates
-	const coordinates: string = est.address.coordinates;
-	const filteredCoordinates: string = coordinates.replace(/ /g,'');
 
     return (
 		<>
 			<Layout page="establishment" title={`${est.name} | Holidaze`} description={est.description}>
 
 				{/* Gallery: */}
-				<section className="establishment-gallery">
-					{formattedGallery} 
-				</section>
+				<EstablishmentGallery gallery={est.gallery} />
 
 				{/* Header: */}
-				<header className="establishment-header">
-					<div className="establishment-header__box">
-						<h2 className="establishment-header__title">{est.name}</h2>
-						<div className="establishment-header__stars">{calculatedStars}</div>
-					</div>
-					<div className="establishment-header__box">
-						<div className="establishment-header__address">
-							<Image src="/assets/icons/address-primary.svg" alt="star" layout="intrinsic" width={15} height={15} />
-							{est.address.street}
-						</div>
-						<div className="establishment-header__rating-box">
-							<div>
-								<div className="establishment-header__rating">{est.rating}</div>
-								<div className="establishment-header__reviews">{est.reviews} reviews</div>
-							</div>
-						</div>
-					</div>
-				</header>
+				<EstablishmentHeader 
+					name={est.name}
+					street={est.address.street}
+					stars={est.stars}
+					reviews={est.reviews}
+					rating={est.rating} />
 
-				{/* Details: */}
-				<section className="establishment-details">
-					<Details 
-						coordinates={filteredCoordinates}
-						description={est.description}
-						amenities={est.amenities} />
-					<Rooms />
-					<EstablishmentReviews />
-				</section>
+				{/* Information: */}
+				<EstablishmentInfo
+					coordinates={est.address.coordinates} 
+					description={est.description}
+					amenities={est.amenities} />  
 
 				{/* Booking */}
 				<EstablishmentCTA />
 				
 			</Layout>
 
+
 			{/* Booking Enquiry Modal: */}
-			<BookingEnquiry
+			<Booking
 				email={est.email}
 				phone={est.phone}
 				street={est.address.street}
 				city={est.address.city}
 				zipcode={est.address.zipcode}
 				establishment={est.name} />
+
+			{/* Booking Feedback */}
+			<BookingFeedbackSuccess />
+			<BookingFeedbackError />
 		</>
     );
 }
