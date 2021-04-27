0788ec7fa030a151e32dd9bb02ebeff5dc98ca97
Emizzari <emilie@tonnessen.com>
2021-04-27 14:10:16 +0200

    Cleaning up code

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { Provider } from "react-redux";
+
 import { store } from '../../store/redux';
 import Footer from "./Footer"
 import Main from "./Main"
@@ -12,29 +13,26 @@ interface LayoutProps {
     description: string;
 }
 
-const Layout: React.FC<LayoutProps> = ({children, page, title, description }) => {
-    return (
-        <Provider store={store}>
-            <Head>
-                <script src="https://kit.fontawesome.com/0011017bbe.js" crossOrigin="anonymous"></script>
-                <link rel="icon" href="/favicon.ico"  type="image/gif" sizes="16x16"></link>
+const Layout: React.FC<LayoutProps> = ({children, page, title, description }) =>  (
+    <Provider store={store}>
+
+        <Head>
+            <script src="https://kit.fontawesome.com/0011017bbe.js" crossOrigin="anonymous"></script>
+            <link rel="icon" href="/favicon.ico"  type="image/gif" sizes="16x16"></link>
 				<meta name="description" content={description} />
 				<title>{title}</title>
-            </Head>
-            <div className="layout">
-                <div className="wrapper">
-                    <Navigation active={page}/>
+        </Head>
+
+        <div className="layout">
+            <div className="wrapper">
+                <Navigation active={page}/>
                     
-                    <Main page={page}>
-                        {children}
-                    </Main>
-                </div>
-                
-                <Footer />
+                <Main page={page}>{children}</Main>
             </div>
-        </Provider>
-        
-    );
-}
+                
+            <Footer />
+        </div>
+    </Provider>
+);
 
 export default Layout;
