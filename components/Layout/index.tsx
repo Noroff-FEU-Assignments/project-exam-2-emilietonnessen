9d94876acabb4e3c8a03e559c85ff15e55c8da9f
Emizzari <emilie@tonnessen.com>
2021-04-27 14:02:17 +0200

    Adding <head> tag to the component

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { Provider } from "react-redux";
 import { store } from '../../store/redux';
 import Footer from "./Footer"
@@ -7,11 +8,19 @@ import Navigation from "./Navigation"
 interface LayoutProps {
     children: React.ReactNode;
     page: string;
+    title: string;
+    description: string;
 }
 
-const Layout: React.FC<LayoutProps> = ({children, page}) => {
+const Layout: React.FC<LayoutProps> = ({children, page, title, description }) => {
     return (
         <Provider store={store}>
+            <Head>
+                <script src="https://kit.fontawesome.com/0011017bbe.js" crossOrigin="anonymous"></script>
+                <link rel="icon" href="/favicon.ico"  type="image/gif" sizes="16x16"></link>
+				<meta name="description" content={description} />
+				<title>{title}</title>
+            </Head>
             <div className="layout">
                 <div className="wrapper">
                     <Navigation active={page}/>
