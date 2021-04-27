04f72305ba51deca41bb26971c56ae1561145fb0
Emizzari <emilie@tonnessen.com>
2021-04-27 14:01:52 +0200

    Update Modal.tsx

--- components/UI/Modal.tsx
+++ components/UI/Modal.tsx
@@ -1,11 +1,13 @@
 interface ModalProps {
     children: React.ReactNode;
     link: string;
+    id: string;
+    name: string;
 }
 
-const Modal: React.FC<ModalProps> = ({children, link}) => {
+const Modal: React.FC<ModalProps> = ( {children, link, id, name} ) => {
     return (
-        <div className="modal" id="modal">
+        <div className={`modal ${name}`} id={id}>
             <div className="modal__content">
                 <a href={link} className="modal__close">&times;</a>
                 {children}
