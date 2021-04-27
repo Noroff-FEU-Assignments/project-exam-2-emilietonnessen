d5dabd9e53d40d913804a29ca8057b48eef68546
Emizzari <emilie@tonnessen.com>
2021-04-27 13:59:15 +0200

    Creating Meta tags in the constans folder

---
+++ constants/meta.ts
@@ -0,0 +1,8 @@
+export const TITLE_HOME: string = "Online booking of hotels, B&Bs and Guesthouses in Bergen! | Holidaze";
+export const META_HOME: string = "Explore unique accommodation alternatives in Bergen! At Holidaze you can book Hotels, Beds & Breakfasts, and Guesthouses online. This is where memories last!";
+
+export const TITLE_ESTABLISHMENTS: string = "Establishments | Hotels, B&Bs and Guesthouses - Holidaze";
+export const META_ESTABLISHMENTS: string = "Unique and breathtaking Hotels, Beds & Breakfasts, and Guesthouses in Bergen. Start planning your journey now!";
+
+export const TITLE_CONTACT: string = "Contact | Holidaze";
+export const META_CONTACT: string = "Trust and safety. Contact Holidaze for more information and help. We are here for you!";
