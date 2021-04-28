dd9733d424fc141f5a27758443b4a397af64bc1b
Emizzari <emilie@tonnessen.com>
2021-04-28 10:55:45 +0200

    Main structure of Contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,17 +1,23 @@
 import { META_CONTACT, TITLE_CONTACT } from '../constants/meta';
 import Layout from '../components/Layout';
+import { ContactFeedbackSuccess, ContactFeedbackError } from '../components/Contact/ContactFeedback';
+import ContactInfo from '../components/Contact/ContactInfo';
+import ContactForm from '../components/Contact/ContactForm';
 
-const contact: React.FC = () => {
-    return (
-        <>
-            <Layout page="contact" title={TITLE_CONTACT} description={META_CONTACT}>
-                <div className="contact-background"></div>
-                <div className="contact-info">
-                    
-                </div>
-            </Layout>
-        </>
-    );
-}
+const contact: React.FC = () => (
+    <>
+        <Layout page="contact" title={TITLE_CONTACT} description={META_CONTACT}>
+            <div className="contact-background" />    
+
+            <div className="contact-container">
+                <ContactInfo />
+                <ContactForm />
+            </div>
+                
+        </Layout>
+        <ContactFeedbackSuccess />
+        <ContactFeedbackError />
+    </>
+);
 
 export default contact;
