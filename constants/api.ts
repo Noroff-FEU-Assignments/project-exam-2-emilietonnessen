6371ccc5ce99f64f3dcfb6ec3ca7cdd741eae740
Emizzari <emilie@tonnessen.com>
2021-04-28 10:55:52 +0200

    Adding contact url

--- constants/api.ts
+++ constants/api.ts
@@ -2,3 +2,4 @@ export const BASE_URL: string = 'https://secure-headland-54144.herokuapp.com/';
 export const GRAPHQL_URL: string = 'https://secure-headland-54144.herokuapp.com/graphql';
 export const ESTABLISHMENTS_URL: string = 'https://secure-headland-54144.herokuapp.com/establishments';
 export const ENQUIRY_URL: string = 'https://secure-headland-54144.herokuapp.com/enquiries';
+export const CONTACT_URL: string = 'https://secure-headland-54144.herokuapp.com/contact-messages';
