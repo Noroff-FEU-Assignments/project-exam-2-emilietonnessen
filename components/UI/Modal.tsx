e52d7c63119201892e0a4ddb66a638da237f8330
Emizzari <emilie@tonnessen.com>
2021-04-29 10:52:35 +0200

    Trying to work with the login

--- components/UI/Modal.tsx
+++ components/UI/Modal.tsx
@@ -2,12 +2,12 @@ interface ModalProps {
     children: React.ReactNode;
     link: string;
     id: string;
-    name: string;
+    classes: string;
 }
 
-const Modal: React.FC<ModalProps> = ( {children, link, id, name} ) => {
+const Modal: React.FC<ModalProps> = ( {children, link, id, classes} ) => {
     return (
-        <div className={`modal ${name}`} id={id}>
+        <div className={`modal ${classes}`} id={id}>
             <div className="modal__content">
                 <a href={link} className="modal__close">&times;</a>
                 {children}
@@ -17,3 +17,36 @@ const Modal: React.FC<ModalProps> = ( {children, link, id, name} ) => {
 }
 
 export default Modal;
+
+
+interface ModalSuccessProps {
+    link: string;
+}
+
+export const ModalSuccess: React.FC<ModalSuccessProps> = ({link}) => {
+    return (
+        <Modal link={link} id="modal-success" classes="modal-success">
+            Success
+        </Modal>
+    );
+}
+
+interface ModalErrorProps {
+    link: string;
+    errorMessage: string;
+    errorTitle: string;
+}
+
+
+export const ModalError: React.FC<ModalErrorProps> = ({ link, errorMessage, errorTitle }) => {
+    return (
+        <Modal link={link} id="modal-error" classes="modal-error">
+            <h3 className="modal-error__title">
+                {errorTitle}
+            </h3>
+            <p className="modal-error__message">
+                {errorMessage}
+            </p>
+        </Modal>
+    );
+}
--- pages/login.tsx
+++ pages/login.tsx
@@ -6,27 +6,74 @@ import { yupResolver } from "@hookform/resolvers/yup.d";
 import { useState } from "react";
 
 import Layout from '../components/Layout';
-import Button from '../components/UI/Button';
+import {SubmitButton} from '../components/UI/Button';
 import Input from '../components/UI/Form/Input';
 import { TITLE_LOGIN, META_LOGIN } from '../constants/meta';
 import { Logo } from '../components/UI/Icons';
+import { ModalError, ModalSuccess } from '../components/UI/Modal';
+import { useRouter, NextRouter } from 'next/router';
+import { useContext } from "react";
+import AuthContext from "../context/AuthContext";
+import axios from "axios";
+import { BASE_URL } from '../constants/api';
+
+const url: string = BASE_URL + "auth/local/";
 
 interface Schema extends Asserts<typeof schema> {}
 
 const schema = yup.object().shape({
-    username: 
-        yup.string()
-        .required('Please enter your first name')
-        .min(2, 'Please enter 5 or more letters'),
-    password:
-        yup.string()
-        .required('Please enter a Email address'),
+    identifier: yup.string().required('Please a username'),
+    password: yup.string().required('Please a password'),
 });
 
+
+
+
 const login = () => {
+    const [submitting, setSubmitting] = useState(false);
+	const [loginError, setLoginError] = useState(null);
+
+    const router: NextRouter = useRouter();
+
     const { register, handleSubmit, watch, errors } = useForm({
         resolver: yupResolver(schema)
     });
+
+    const [, setAuth]: ((setAuth: {}) => void)[] = useContext(AuthContext);
+
+    
+
+    const onSubmit: (data: {}) => Promise<void>  = async (data) => {
+        setSubmitting(true);
+
+        const options = {
+            method: "POST",
+            body: data,
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }
+        
+        
+
+        console.log("1: onSubmit activated");
+        console.log("2: [Data]", data);
+        try {
+            console.log("3: Trying to get a response from the URL");
+			const response = await axios.post('http://localhost:1337/auth/local', options);
+			console.log("4: [Response]", response.data);
+            console.log(response)
+			setAuth(response.data);
+            
+			//router.push("/admin");
+            console.log('5: Data went through')
+		} catch (error) {
+			console.log("6: [Error]", error);
+			setLoginError(error.toString());
+		} finally {
+			setSubmitting(false);
+		}
+    }
 
     return (
         <>
@@ -37,50 +84,62 @@ const login = () => {
 			<title>{TITLE_LOGIN}</title>
         </Head>
 
-        <div className="login">
-            <div className="login__box">
-                <div className="login__brand">
-                    <div className="login__logo-icon">
-                        <Logo color="#ff1447" />
-                    </div>
-                    <h1 className="login__logo">Holidaze</h1>
-                </div>
+        <div className="login" id="login">
+            <form onSubmit={handleSubmit(onSubmit)}>
 
-                <div className="login__fields">
-                    <Input
-                        register={register}
-                        name="username"
-                        label="Username"
-                        type="text"
-                        error={errors.username && <span className="form__error"><i className="fas fa-exclamation-circle"></i> {errors.username.message}</span>}
-                        placeholder="noranordmann" />
+                
 
-                    <Input
-                        register={register}
-                        name="password"
-                        label="Password"
-                        type="text"
-                        error={errors.password && <span className="form__error"><i className="fas fa-exclamation-circle"></i> {errors.password.message}</span>}
-                        placeholder="********" />
-
-                    <div className="login__essentials">
-                        <div className="login__remember">
-                            <input type="checkbox" name="remember" id="remember"/>
-                            <label htmlFor="checkbox">Remember me</label>
+                <fieldset disabled={submitting} className="form__fieldset login__box">
+                    <div className="login__brand">
+                        <div className="login__logo-icon">
+                            <Logo color="#ff1447" />
                         </div>
-                        <a className="login__forgot" href="#forgot">
-                            Forgot Password?
-                        </a>
+                        <h1 className="login__logo">Holidaze</h1>
                     </div>
-                </div>
 
-                <div className="login__button">
-                    <Button link="/" theme="primary" size="md">
-                        login
-                    </Button>
-                </div>
-            </div>
+                    <div className="login__fields">
+
+                        {/* Display an error */}
+                        {loginError && <div className="form__feedback--error">{loginError}</div>}
+                        <Input
+                            register={register}
+                            name="identifier"
+                            label="Username"
+                            type="text"
+                            error={errors.username && <span className="form__error"><i className="fas fa-exclamation-circle"></i> {errors.username.message}</span>}
+                            placeholder="noranordmann" />
+
+                        <Input
+                            register={register}
+                            name="password"
+                            label="Password"
+                            type="password"
+                            error={errors.password && <span className="form__error"><i className="fas fa-exclamation-circle"></i> {errors.password.message}</span>}
+                            placeholder="********" />
+
+                        <div className="login__essentials">
+                            <div className="login__remember">
+                                <input type="checkbox" name="remember" id="remember"/>
+                                <label htmlFor="checkbox">Remember me</label>
+                            </div>
+                            <a className="login__forgot" href="#forgot">
+                                Forgot Password?
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="login__button">
+                        <SubmitButton  theme="primary" size="md">
+                            {submitting ? "loggin in..." : "login"}
+                        </SubmitButton>
+                    </div>
+                </fieldset>
+                
+            </form>
+
         </div>
+
+        <ModalError link="#login" errorMessage="Please try again!" errorTitle="An error occured" />
         </>
     );
 }
