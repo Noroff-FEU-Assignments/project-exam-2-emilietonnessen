773060e85a9f34f0d6ce33a951e515f6656c0c8c
Emizzari <emilie@tonnessen.com>
2021-04-30 16:06:37 +0200

    Create EditEstablishments.tsx

---
+++ components/AdminDashboard/EditEstablishments.tsx
@@ -0,0 +1,13 @@
+import Accordion from '../UI/Accordion';
+
+const EditEstablishments: React.FC = () => {
+    return (
+        <section className="edit-establishments">
+            <Accordion title="Add Establishment">
+                All the messages!
+            </Accordion>  
+        </section>
+    );
+}
+
+export default EditEstablishments;
