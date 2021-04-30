d3bb8db8b7c9585eac760725a753e361f0b4efa3
Emizzari <emilie@tonnessen.com>
2021-04-30 16:06:41 +0200

    Create ContactNotifications.tsx

---
+++ components/AdminDashboard/ContactNotifications.tsx
@@ -0,0 +1,13 @@
+import Accordion from '../UI/Accordion';
+
+const ContactNotifications: React.FC = () => {
+    return (
+        <section className="contact-notifications">
+            <Accordion title="Contact Messages ">
+                All the messages!
+            </Accordion>     
+        </section>
+    );
+}
+
+export default ContactNotifications;
