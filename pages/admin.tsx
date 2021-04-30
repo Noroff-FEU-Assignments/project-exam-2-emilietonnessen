cc3da5ded4a9bab50ec393f41c9f7a974b165a0e
Emizzari <emilie@tonnessen.com>
2021-04-30 16:05:11 +0200

    Redirecting if a user is trying to cheat themself in

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,6 +1,32 @@
-import AdminLayout from "../components/Layout/AdminLayout";
-import { META_ADMIN, TITLE_ADMIN } from "../constants/meta";
+import { NextRouter, useRouter } from "next/router";
+import { useContext, useEffect } from "react";
 
+import { META_ADMIN, TITLE_ADMIN } from "../constants/meta";
+import AdminDashboard from "../components/AdminDashboard";
+import AdminLayout from "../components/Layout/AdminLayout";
+import AuthContext from "../context/AuthContext";
+
+const admin: React.FC = () => {
+    const [auth]: any = useContext(AuthContext);
+    const router: NextRouter = useRouter();
+
+    // Redirect the user if the user is not authenticated or if there is no auth in the local storage
+    useEffect(() => {
+        if(!auth) {
+            router.push("/login");
+            localStorage.clear();
+        } 
+
+        try {
+            if (auth.user.role.type !== "authenticated") {
+                router.push("/login");
+                localStorage.clear();
+            }
+        } catch (error) {
+            router.push("/login");
+            localStorage.clear();
+        }
+    }, []);
 
 const admin = () => {
     return (
