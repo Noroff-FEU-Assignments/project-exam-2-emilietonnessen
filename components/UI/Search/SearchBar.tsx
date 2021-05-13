25aeb146677a34c69acc237906f18a3f22ef4d53
Emizzari <emilie@tonnessen.com>
2021-05-13 14:54:53 +0200

    Cleaning up Searchbar

--- components/UI/Search/SearchBar.tsx
+++ components/UI/Search/SearchBar.tsx
@@ -1,25 +1,19 @@
-import Image from "next/image"
+import * as icons from '../Icons';
 import * as interfaces from '../../../constants/interfaces';
 
 const SearchBar: React.FC<interfaces.SearchBarProps> = ({theme, search, value, clearSearch, iconType}) => (
+    <div className="search__bar">
+        <input 
+            type="text" 
+            className={theme ?  `search__input search__input--${theme}` : 'search__input'} 
+            placeholder="Search.."
+            onChange={search}
+            value={value} />
 
-    
-    return (
-        <div className="search__bar">
-            <input 
-                type="text" 
-                className={theme ?  `search__input search__input--${theme}` : 'search__input'} 
-                placeholder="Search.."
-                onChange={search}
-                value={value}
-            />
-            <button 
-                className="search__button"
-                onClick={clearSearch} >
-                <Image src="/assets/search.svg" alt="button" width="16" height="16"  />
-            </button>
-        </div>
-    );
-}
+        <button className="search__button" onClick={clearSearch} >
+            {iconType ? <icons.Cross /> : <icons.Search />} 
+        </button>
+    </div>
+);
 
 export default SearchBar;
